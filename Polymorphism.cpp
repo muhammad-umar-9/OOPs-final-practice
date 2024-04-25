548c1aa67f637a9b867903a2f3c384ed4e5be8f0
muhammad-umar-9 <muhammadumark900@gmail.com>
2024-04-25 19:51:09 +0500

    binding

--- Polymorphism.cpp
+++ Polymorphism.cpp
@@ -1,6 +1,26 @@
 #include<iostream>
 using namespace std;
+class Base
+{
+      public:
+      void Show()
+      {
+            cout<<"In Base Class"<<endl;
+      }
+};
+class Derived: public Base
+{
+    public:
+      void Show()
+      {
+            cout<<"In Derived Class";
+      }  
+};
 int main()
 {
-      cout<<"Hello World";
+      Base b;
+      Derived d;
+      b.Show();
+      d.Show();
+      
 }
