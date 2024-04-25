4afa70fe6abc4be9924538b76ef4c59d77075cf0
muhammad-umar-9 <muhammadumark900@gmail.com>
2024-04-25 20:02:53 +0500

    Pointer_binding

---
+++ Pointer_binding.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+class Base
+{
+      public:
+      void Show()
+      {
+            cout<<"In Base Class"<<endl;
+      }
+};
+class Derived: public Base
+{
+    public:
+      void Show()
+      {
+            cout<<"In Derived Class";
+      }  
+};
+int main()
+{
+      system("cls");
+      Base *b;
+      Derived d;
+      b = &d;
+      b->Show();
+
+      return 0;
+}
